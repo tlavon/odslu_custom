fb2d94fdada71f49c4dc0a3889cf4ab399fde526
Tanessia Gordon <>
2021-06-08 15:56:14 -0400

    upgrade to v 1.2

--- wp-content/themes/odslu-custom-theme/header.php
+++ wp-content/themes/odslu-custom-theme/header.php
@@ -19,7 +19,7 @@
                         <li><a href="<?php echo site_url('/one-day-she-leveled-up') ?>">ODSLU</a></li>
                         <li><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
                         <li><a href="<?php echo site_url() ?>"><img class="logo" src="<?php echo get_theme_file_uri('/images/odslu-logo.png') ?>" alt=""></a></li>
-                        <li><a href="#">Shop</a></li>
+                        <li><a href="<?php echo site_url('/shop') ?>">Shop</a></li>
                         <li><a href="<?php echo site_url('/about') ?>">About</a></li>
                         <li><a href="<?php echo site_url('/contact-us') ?>">Contact</a></li>
                     </ul>
